aca6c08a0d75a2956d448f233d7a651bc6f2bd15
Manuel Ibar <manuel.ibar@accenture.com>
2020-04-25 23:57:18 -0300

    Seteo de Redux para el nick del User

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import createRoutes from './Routes';
+import MyReduxStore from "./redux/MyReduxStore"
+// import * as serviceWorker from './serviceWorker';
+
+import "./Base.css"
+
+//carga de todas mis rutas
+const routes = createRoutes();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = { MyReduxStore }>
+      <Router>
+        {routes}
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
